bd8d9f7e01ef1b5361a22e9ad3ea9a9663b40361
robert <matthewrueter51@gmail.com>
2023-01-12 16:12:47 -0600

    create short and long classes for bricks and assign randomly on render.

--- src/BrickWall.js
+++ src/BrickWall.js
@@ -3,30 +3,38 @@ import './index.css';
 let size = selectSize();
 function selectSize (){
 	let size =[]
-	const number = Math.floor(Math.random() * 100);
+	const number = Math.floor(Math.random() * 500);
 	for(let i = 0; i < number; i++){
 		size.push(i);
 	}
 	return size;
 }
 
-function selectColor (){
+function selectClass (){
 	let color = ""
-	const number =Math.floor(Math.random() * 10);
-	if(number < 5){
-		color = "brickDark"
-	}else if(number >= 5){
-		color = "brickLight"
+	const number =Math.floor(Math.random() * 20);
+	if(number >=0 && number <=5){
+		color = "brickDarkShort"
+	}else if(number > 5 && number <=10){
+		color = "brickLightShort"
+	}else if(number > 10 && number <=15){
+		color = "brickDarkLong"
+	}else if(number > 15 && number <=20){
+		color = "brickLightLong"
+	}else{
+		color ="brickError"
+		console.log(number)
 	}
 	return color;
 }
 
 
 export default function BrickWall (){
+	
 	const bricks = size.map((brick)=>{
 		let key =brick;
 		return(
-			<Brick key={key} color = {selectColor()} />
+			<Brick key={key} color = {selectClass()} />
 		)
 	});
 
