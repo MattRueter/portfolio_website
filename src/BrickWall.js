3bcbd7748926bb2695ab92d2061734bd20cadcc4
robert <matthewrueter51@gmail.com>
2023-01-12 15:44:28 -0600

    create random color selector function and pass to Brick component.

--- src/BrickWall.js
+++ src/BrickWall.js
@@ -1,78 +1,28 @@
 import './index.css';
 
+
+function selectColor (){
+	let color = ""
+	const number =Math.floor(Math.random() * 10);
+	if(number < 5){
+		color = "brickDark"
+	}else if(number >= 5){
+		color = "brickLight"
+	}
+	return color;
+}
+
+
 export default function BrickWall (){
 	return (
 		<div id="brickWall">
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
-			<Brick />
+			<Brick color={selectColor()}/>
 		</div>
 	)
 }
 
-function Brick(){
+function Brick({color}){
 	return (
-		<div className="brickDark"></div>
+		<div className={color}></div>
 	)
 }
