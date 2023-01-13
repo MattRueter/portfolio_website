93b5880b99c790e14b58c140bc8cdfb1a99cfd27
robert <matthewrueter51@gmail.com>
2023-01-13 16:16:13 -0600

    create Route for home and background page and hook up to index.js.

---
+++ src/Background_page.js
@@ -0,0 +1,10 @@
+
+export default function BackgroundPage (){
+	return (
+		<div>
+			<h1>A little about me.</h1>
+			<h2>lalalalalalalalalala</h2>
+			<p>And some more details if anyone is interested.</p>
+		</div>
+	)
+}
--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,10 +1,14 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
+import BackgroundPage from "./Background_page.js";
+import App from "./App.js";
+
 
 export default function RouteSwitch () {
 	return (
 		<BrowserRouter>
 			<Routes>
-				//Route path here (background)
+				<Route path="/" element={<App/>}/>
+				<Route path="/background" element={<BackgroundPage />}/>
 				//Route path here (portfolio)
 				//Route path here (anti-portfolio)
 				//Route path here (blog)
