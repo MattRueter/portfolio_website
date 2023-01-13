17fab6cade052b17f9c15a3252ac7af34bbbc6be
robert <matthewrueter51@gmail.com>
2023-01-13 16:07:42 -0600

    create RouteSwitch file and skeleton router component.

---
+++ src/RouteSwitch.js
@@ -0,0 +1,15 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+export default function RouteSwitch () {
+	return (
+		<BrowserRouter>
+			<Routes>
+				//Route path here (background)
+				//Route path here (portfolio)
+				//Route path here (anti-portfolio)
+				//Route path here (blog)
+				//Route path here (other things about me)
+			</Routes>
+		</BrowserRouter>
+	)
+}
