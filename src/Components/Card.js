503c4f180adcae2e7b5e5bc7131b39a11aa077ca
robert <matthewrueter51@gmail.com>
2023-03-06 11:49:05 -0600

    Create basic layout for blog page.

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -19,3 +19,14 @@ export const InfoCard = ({id,cardContent}) => {
 		</div>
 	)
 }
+export const BlogCard = ({id,cardContent}) => {	
+	const openBlog = () => {
+		console.log("opening article.")
+	};
+	return (
+		<div className={"card"} id={id} onClick={openBlog}>
+			<h1>{cardContent.title}</h1>
+			<p>{cardContent.description}</p>
+		</div>
+	)
+}
