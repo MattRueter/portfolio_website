54ee8c299d66b447c071dac73c60bd8009c917f1
robert <matthewrueter51@gmail.com>
2023-03-06 15:20:47 -0600

    Create Title component.

---
+++ src/Components/Title.js
@@ -0,0 +1,3 @@
+export const Title = ({content}) => {
+	return	<h1 id={content.id}className={"title"}>{content.title}</h1>
+}
--- src/Pages/AboutMe_Page.js
+++ src/Pages/AboutMe_Page.js
@@ -1,5 +1,6 @@
 import { Navbar } from "../Components/Navbar"
 import { InfoCard } from "../Components/Card";
+import { Title } from "../Components/Title";
 
 
 const cardContent = [
@@ -25,8 +26,35 @@ const cardContent = [
 		Nunc non purus ac elit maximus iaculis viverra sit amet libero. 
 		Sed sed porttitor ante. Mauris accumsan est eget elit vehicula, in finibus sem rhoncus. 
 		Aliquam erat volutpat.`,
+	},
+	{
+		id:"goals",
+		description:`Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+		Duis nec nisi dictum, tincidunt eros in, tempor mi. 
+		Donec in vulputate neque, eu accumsan ante. Fusce eu ex vitae lectus lacinia commodo eget faucibus turpis. 
+		Duis posuere orci id egestas elementum. `
+	},
+	{
+		id:"skills",
+		description:`Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+		Duis nec nisi dictum, tincidunt eros in, tempor mi. 
+		Donec in vulputate neque, eu accumsan ante. Fusce eu ex vitae lectus lacinia commodo eget faucibus turpis. 
+		Duis posuere orci id egestas elementum. `
 	}
-
+]
+const titleList = [
+	{
+		id:"title0",
+		title:"What I'm Looking For:"
+	},
+	{
+		id:"title1",
+		title: "Background"
+	},
+	{
+		id:"title2",
+		title: "Skills"
+	}
 ]
 
 export default function BackgroundPage (){
@@ -36,11 +64,22 @@ export default function BackgroundPage (){
 		)	
 	});
 
+	const titles = titleList.map((title,index) =>{
+		return (
+			<Title content={title} key={index}/>
+		)
+	});
 	return (
 		<div>
 			<Navbar className={"navPages"} currentPage={"/background"} iconBoxClass={"iconBoxPages"} navBarLinksClass={"linksBoxPages"}/>
 			<main className={"CardContainer"}>
-				{infocards}
+				{titles[0]}
+				{infocards[0]}
+				{titles[1]}
+				{infocards[1]}
+				{infocards[2]}
+				{titles[2]}
+				{infocards[3]}
 			</main>
 		</div>
 	)
