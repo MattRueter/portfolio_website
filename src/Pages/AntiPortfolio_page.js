51d167d4bd066f71f9f4795d55893883183fdce9
robert <matthewrueter51@gmail.com>
2023-03-06 20:01:18 -0600

    Update content on Anti portfolio cards.

--- src/Pages/AntiPortfolio_page.js
+++ src/Pages/AntiPortfolio_page.js
@@ -4,8 +4,10 @@ import { InfoCard, ProjectCard } from "../Components/Card";
 const infoCard =[
 	{
 		id:"antiPortfolioInfo",
-		description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-		Duis nec nisi dictum, tincidunt eros in, tempor mi.`,
+		description: `I've included this as I find it edifying to reflect on earlier projects and understand why they were 
+		good or bad and how they could be improved. These two projects were both for different reasons two I really enjoyed 
+		working on, however when I look at them now they make me cringe! But working on them set me up to better understand and 
+		appreciate subsequent concepts in programming and specifically how Javascript works.`,
 	}
 ]
 const projectList = [
@@ -13,18 +15,14 @@ const projectList = [
 	{
 		id:"5",
 		projectName:"Guess -who",
-		image:"an image here",
-		description:`Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-		Duis nec nisi dictum, tincidunt eros in, tempor mi.`,
+		description:`Genre: game`,
 		siteLink: "url here",
 		codeLink:"url here",
 	},
 	{
 		id:"6",
 		projectName:"Library",
-		image:"an image here",
-		description:`Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-		Duis nec nisi dictum, tincidunt eros in, tempor mi.`,
+		description:`Genre: personal organization tool`,
 		siteLink: "url here",
 		codeLink:"url here",
 	},
